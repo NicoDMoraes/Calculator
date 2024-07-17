4bf132b9afa98026704c10643913fb33f280b446
Nicolau <nicodinizmoraes@gmail.com>
2024-07-17 20:45:56 -0300

    fix: removed some unused divs

--- src/components/calculatorFrame.tsx
+++ src/components/calculatorFrame.tsx
@@ -4,18 +4,16 @@ import './calculatorFrame.css';
 export function CalculatorFrame() {
     return (
         <div class="calculator-frame">
-            <div class="calculator-frame-content">
-                <div class="calculator-frame-content-top">
-                    <div class="calculator-frame-content-top-left">
-                        calc
-                    </div>
-                    <div class="calculator-frame-content-top-right">
-                        <div class="frame-selector">
-                            <div>theme</div>
-                            <button class="frame-selector-button">1</button>
-                            <button class="frame-selector-button">2</button>
-                            <button class="frame-selector-button">3</button>
-                        </div>
+            <div class="calculator-frame-content-top">
+                <div class="calculator-frame-content-top-left">
+                    calc
+                </div>
+                <div class="calculator-frame-content-top-right">
+                    <div class="frame-selector">
+                        <div>theme</div>
+                        <button class="frame-selector-button">1</button>
+                        <button class="frame-selector-button">2</button>
+                        <button class="frame-selector-button">3</button>
                     </div>
                 </div>
             </div>
