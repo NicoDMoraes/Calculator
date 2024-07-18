752abcf5de5ca4f51d5dfd889e5ef45fff1c3b8b
Nicolau <nicodinizmoraes@gmail.com>
2024-07-17 21:56:35 -0300

    feat: basic onClick added with createSignal

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,12 +1,14 @@
 
 import './calculator.css';
-import { createSignal } from 'solid-js';
+import { createSignal, createEffect } from 'solid-js';
 import './buttons.css';
 
 
 export function CalculatorFrame() {
-    const [display, setDisplay] = createSignal(0);
-    const Display: number = display();
+    const [display, setDisplay] = createSignal('');
+    createEffect(() => {
+        console.log(display());
+    });
     return (
         <div class="calculator-frame">
             <div class="calculator-frame-content-top">
@@ -23,7 +25,7 @@ export function CalculatorFrame() {
                 </div>
             </div>
             <div class="calculator-frame-content-middle">
-                <div class="calculator-frame-content-middle-display">{Display}</div>
+                <div class="calculator-frame-content-middle-display">{display() === '' ? 0 : display()}</div>
             </div>
             <div class="calculator-frame-content-bottom">
                 <div class="buttons">
@@ -32,29 +34,29 @@ export function CalculatorFrame() {
                             <button onClick={() => setDisplay(display() + 7)} class="button number">7</button>
                             <button onClick={() => setDisplay(display() + 8)} class="button number">8</button>
                             <button onClick={() => setDisplay(display() + 9)} class="button number">9</button>
-                            <button class="button del">DEL</button>
+                            <button onClick={() => setDisplay(display().slice(0, -1))} class="button del">DEL</button>
                         </div>
                         <div class="row 2">
                             <button onClick={() => setDisplay(display() + 5)} class="button number">5</button>
                             <button onClick={() => setDisplay(display() + 4)} class="button number">4</button>
                             <button onClick={() => setDisplay(display() + 6)} class="button number">6</button>
-                            <button class="button add">+</button>
+                            <button onclick={() => setDisplay(display() + '+')} class="button add">+</button>
                         </div>
                         <div class="row 3">
                             <button onClick={() => setDisplay(display() + 1)} class="button number">1</button>
                             <button onClick={() => setDisplay(display() + 2)} class="button number">2</button>
                             <button onClick={() => setDisplay(display() + 3)} class="button number">3</button>
-                            <button class="button sub">-</button>
+                            <button onClick={() => setDisplay(display() + '-')} class="button sub">-</button>
                         </div>
                         <div class="row 4">
-                            <button class="button ponctuation">.</button>
+                            <button onClick={() => setDisplay(display() + '.')} class="button ponctuation">.</button>
                             <button onClick={() => setDisplay(display() + 0)} class="button number">0</button>
-                            <button class="button div">/</button>
-                            <button class="button mul">x</button>
+                            <button onClick={() => setDisplay(display() + '/')} class="button div">/</button>
+                            <button onClick={() => setDisplay(display() + '*')} class="button mul">x</button>
                         </div>
                     </div>
                     <div class="big">
-                        <button class="big-button reset">RESET</button>
+                        <button onClick={() => setDisplay('')} class="big-button reset">RESET</button>
                         <button class="big-button equals">=</button>
                     </div>
                 </div>
